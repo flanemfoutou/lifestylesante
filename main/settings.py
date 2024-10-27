6ed52f4f6e35cdb1575187b79871180f0e1e1a0d
ned <exaucengango20gmail.com>
2024-10-27 15:35:18 +0100

    remettre la config static

--- main/settings.py
+++ main/settings.py
@@ -123,11 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_URL = 'static/'
 
-MEDIA_URL = '/medias/'
-MEDIA_ROOT = '/medias/'
+MEDIA_URL = 'medias/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_ROOT = BASE_DIR / 'medias'
 
 
 # Default primary key field type
