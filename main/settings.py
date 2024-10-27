d481ee2c0c977d2d1433a9f4f8982be9bb5d1b5f
ned <exaucengango20gmail.com>
2024-10-27 20:20:07 +0100

    Okk

--- main/settings.py
+++ main/settings.py
@@ -124,10 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = '/app/static/'
 
 MEDIA_URL = '/medias/'
-MEDIA_ROOT = '/medias/'
+MEDIA_ROOT = '/app/medias/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
