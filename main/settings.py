b5dfa92df0cc28584a7e883eefda757eacf9e2bc
ned <exaucengango20gmail.com>
2024-10-27 09:42:27 +0100

    Revoir la config static

--- main/settings.py
+++ main/settings.py
@@ -124,14 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/static/'
 
 MEDIA_URL = '/medias/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'medias')
+MEDIA_ROOT = '/medias/'
 
-MEDIAFILES_DIRS = [
-    os.path.join(BASE_DIR, "medias")
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
