6210c37146be4ab00dc3961f07c46ee8610f85d6
ned <exaucengango20gmail.com>
2024-10-28 10:26:08 +0100

    static root

--- main/settings.py
+++ main/settings.py
@@ -124,6 +124,7 @@ MEDIA_URL = 'medias/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_ROOT = BASE_DIR / 'medias'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
