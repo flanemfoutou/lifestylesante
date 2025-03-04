d4bda233a4bb62b08fb07ce64ceb1acda5cfba9f
monsieur-ned <exaucengango20@gmail.com>
2025-03-04 21:30:01 +0100

    is_deleted

--- profiles/admin.py
+++ profiles/admin.py
@@ -45,7 +45,7 @@ export_as_excel.short_description = "Exporter en Excel"
 
 # Fonction pour restaurer les objets supprimés
 def restore_selected_objects(modeladmin, request, queryset):
-    queryset.update(isDeleted=False)
+    queryset.update(is_deleted=False)
 
 restore_selected_objects.short_description = "Restaurer les objets sélectionnés"
 
@@ -55,15 +55,15 @@ class BaseAdmin(admin.ModelAdmin):
     Classe de base pour l'administration qui inclut la gestion des objets supprimés.
     """
     actions = [export_as_csv, export_as_excel, restore_selected_objects]  # Ajout des actions
-    list_filter = ('isDeleted',)  # Ajout du filtre pour voir les objets supprimés
+    list_filter = ('is_deleted',)  # Ajout du filtre pour voir les objets supprimés
     
     def get_queryset(self, request):
         """
-        Filtre par défaut pour exclure les objets supprimés, sauf si le filtre isDeleted=True est utilisé.
+        Filtre par défaut pour exclure les objets supprimés, sauf si le filtre is_deleted=True est utilisé.
         """
         qs = super().get_queryset(request)
-        if not request.GET.get('isDeleted__exact') == 'True':
-            qs = qs.filter(isDeleted=False)
+        if not request.GET.get('is_deleted__exact') == 'True':
+            qs = qs.filter(is_deleted=False)
         return qs
 
 #  Admin Employé
@@ -71,13 +71,13 @@ class BaseAdmin(admin.ModelAdmin):
 class EmployeAdmin(BaseAdmin):
     list_display = ('user', 'nom', 'prenom', 'sexe', 'fonction', 'telephone', 'email')
     search_fields = ('nom', 'prenom', 'telephone', 'email', 'fonction')
-    list_filter = ('sexe', 'fonction', 'departement', 'isDeleted')
+    list_filter = ('sexe', 'fonction', 'departement', 'is_deleted')
 
 #  Admin MarquerArrivee
 @admin.register(MarquerArrivee)
 class MarquerArriveeAdmin(BaseAdmin):
     list_display = ('employe', 'date_arrivee', 'arrivee', 'montant')
-    list_filter = ('arrivee', 'date_arrivee', 'isDeleted')
+    list_filter = ('arrivee', 'date_arrivee', 'is_deleted')
     search_fields = ('employe__nom', 'employe__prenom', 'date_arrivee')
     ordering = ('-date_arrivee',)
 
@@ -85,7 +85,7 @@ class MarquerArriveeAdmin(BaseAdmin):
 @admin.register(MarquerDepart)
 class MarquerDepartAdmin(BaseAdmin):
     list_display = ('employe', 'date_depart', 'depart')
-    list_filter = ('depart', 'date_depart', 'isDeleted')
+    list_filter = ('depart', 'date_depart', 'is_deleted')
     search_fields = ('employe__nom', 'employe__prenom', 'date_depart')
     ordering = ('-date_depart',)
 
@@ -93,7 +93,7 @@ class MarquerDepartAdmin(BaseAdmin):
 @admin.register(RapportMensuel)
 class RapportMensuelAdmin(BaseAdmin):
     list_display = ('employe', 'mois', 'annee', 'total_arrivees', 'total_departs', 'total_montant')
-    list_filter = ('mois', 'annee', 'isDeleted')
+    list_filter = ('mois', 'annee', 'is_deleted')
     search_fields = ('employe__nom', 'employe__prenom')
     ordering = ('-annee', '-mois')
 
