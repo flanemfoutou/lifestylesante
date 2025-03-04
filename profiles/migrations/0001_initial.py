362761134ade145da2812375f8268b98f3058778
monsieur-ned <exaucengango20@gmail.com>
2025-03-04 22:18:51 +0100

    migrate

--- profiles/migrations/0001_initial.py
+++ profiles/migrations/0001_initial.py
@@ -56,20 +56,5 @@ class Migration(migrations.Migration):
                 ('date_depart', models.DateTimeField(auto_now_add=True)),
                 ('employe', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='profiles.employe')),
             ],
-        ),
-        migrations.CreateModel(
-            name='RapportMensuel',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('mois', models.IntegerField()),
-                ('annee', models.IntegerField()),
-                ('total_arrivees', models.IntegerField(default=0, verbose_name='Arrivées signalées')),
-                ('total_departs', models.IntegerField(default=0, verbose_name='Départs signalés')),
-                ('total_montant', models.DecimalField(decimal_places=2, default=0.0, max_digits=10, verbose_name='Gain mensuel')),
-                ('employe', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='profiles.employe')),
-            ],
-            options={
-                'unique_together': {('employe', 'mois', 'annee')},
-            },
-        ),
+        )
     ]
